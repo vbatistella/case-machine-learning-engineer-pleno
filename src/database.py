8a788b0e07593b6d11f0a129fe9009b4f91d09b7
vbatistella <victorabatistella@gmail.com>
2024-12-15 10:33:57 -0300

    feat: inference application

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 from mongomock.mongo_client import MongoClient
+from datetime import datetime, timezone
 
 class InMemoryDatabase:
     _instance = None
@@ -7,4 +8,31 @@ class InMemoryDatabase:
         if cls._instance is None:
             client = MongoClient()
             cls._instance = client.get_database('memory_db')
-        return cls._instance
+        return super().__new__(cls)
+    
+    def add_inference(self, payload: dict, score: float, version: float) -> None:
+        """
+        Adds a new inference record to the database, including the input features, predicted score, 
+        timestamp, and model version.
+
+        Args:
+            payload (dict): A dictionary containing the input features used for the model inference.
+            score (float): The predicted score returned by the model.
+            version (float): The version of the model used for generating the prediction.
+        """
+        data = {}
+        data["features"] = payload
+        data["score"] = score
+        data["datetime"] = datetime.now(timezone.utc).isoformat()
+        data["model"] = version
+        self._instance["data_capture"].insert_one(data)
+    
+    def get_history(self) -> list:
+        """
+        Retrieves the history of all recorded inferences from the database.
+
+        Returns:
+            list: A list of dictionaries, where each dictionary represents a record of a previous 
+                  inference
+        """
+        return list(self._instance["data_capture"].find())
--- src/main.py
+++ src/main.py
@@ -1,36 +1,87 @@
-from fastapi import FastAPI
-from database import InMemoryDatabase
-
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from src.database import InMemoryDatabase
+from src.model import InferenceModel
 import uvicorn
-
 
 app = FastAPI()
 
-
-@app.get("/health", status_code=200, tags=["health"], summary="Health check")
+@app.get("/health/", status_code=200, tags=["health"], summary="Health check")
 async def health():
+    """
+    Health check endpoint to monitor the status of the API.
+
+    Returns:
+        dict: A dictionary with a key "status" set to "ok" if the server is running fine.
+    """
     return {"status": "ok"}
 
-@app.post("/user/", tags=["example"], summary="Insert user")
-async def insert(data: dict):
-    db = InMemoryDatabase()
-    users = db.get_collection('users')
-    users.insert_one(data)
-    return {"status": "ok"}
+@app.post("/model/predict/", status_code=200, tags=["prediction"], summary="Model prediction")
+async def predict(payload: dict):
+    """
+    Predicts the outcome based on the provided input data..
 
-@app.get("/user/{name}", status_code=200, tags=["example"], summary="Get user by name")
-async def get(name: str):
-    db = InMemoryDatabase()
-    users = db.get_collection('users')
-    user = users.find_one({"name": name})
-    return {"status": "ok", "user": user}
+    Args:
+        payload (dict): A dictionary containing the input features for prediction.
+                         The keys and values should match the expected format of the model.
+                         Example:
+                         {
+                             "sched_dep_time": 1059,      # Scheduled departure time (in HHMM format)
+                             "sched_arr_time": 1209,      # Scheduled arrival time (in HHMM format)
+                             "distance": 284,             # Distance of the flight in miles
+                             "clouds": 93,                # Cloud cover percentage
+                             "pres": 1011,                # Atmospheric pressure in hPa
+                             "wind_spd": 5.8,             # Wind speed in mph
+                             "precip": 0.0,               # Precipitation amount in inches
+                             "snow": 0.0                  # Snow amount in inches
+                         }
 
-@app.get("/user/", tags=["example"], summary="List all users")
-async def list():
-    db = InMemoryDatabase()
-    users = db.get_collection('users')
-    return {"status": "ok", "users": [x for x in users.find({},{"_id": 0})]}
+    Returns:
+        dict: A dictionary containing the predicted score.
+    """
+    model = InferenceModel()
+    y = model.inference(payload)
+    version = model.get_version()
 
+    db = InMemoryDatabase()
+    db.add_inference(payload, y, version)
+
+    return {"score": y, "model_version": version}
+
+@app.post("/model/load/", status_code=200, tags=["model"], summary="Load model")
+async def load_model(file: UploadFile = File(...)):
+    """
+    Loads a new model from the uploaded .pkl file.
+
+    Args:
+        file (UploadFile): The .pkl file containing the pre-trained model. This file is uploaded
+                           through the API request.
+
+    Returns:
+        dict: A dictionary containing the status of the model loading process.
+    """
+    if not file.filename.endswith(".pkl"):
+        raise HTTPException(status_code=400, detail="Only .pkl files are supported.")
+    
+    contents = await file.read()
+    model = InferenceModel()
+    
+    try:
+        result = model.update_model(contents)
+        return result
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.get("/model/history/", status_code=200, tags=["history"], summary="Check last predictions")
+async def history():
+    """
+    Retrieves the history of the last predictions made by the model.
+    
+    Returns:
+        dict: A dictionary containing a list of the last prediction records.
+    """
+    db = InMemoryDatabase()
+    history = db.get_history()
+    return {"history": history}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
